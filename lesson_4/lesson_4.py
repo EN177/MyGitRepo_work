d048540279920be052a406e4932885de6ecb86d5
EN <ereminn177@gmail.com>
2025-06-15 20:41:37 +0300

    lesson_4

---
+++ lesson_4/lesson_4.py
@@ -0,0 +1,57 @@
+
+# print('-' * 20)
+# print("Copyrights")
+# print('-' * 20)
+
+# symb = '='
+# symb *= 30
+# print(symb)
+# print("Copyrights")
+# print(symb)
+
+# text = 'I love Python!'
+# print('love' in text)
+
+# user = input ('what is yout name?')
+# print('Hello',user,'!')
+
+# my_list = [1, 3, 6, 7, None, 'text', False, 2.42]
+# print(my_list[2])
+# my_list[2] = 42
+# print(my_list)
+
+
+# my_list = []
+# my_list.append(42)
+# my_list.append('text')
+# print(my_list)
+# print(len(my_list))
+# print(my_list.index(42))
+# poped = my_list.pop(0)
+# print(poped)
+# print(42 in my_list)
+
+# my_tuple = (1, 3, 6, 7, None, 'text', False, 2.42)
+
+# my_set = {1, 3, 6, 7, None, 'text', False, 2.42}
+# print(my_set)
+# my_set.add(42)
+# print(my_set)
+
+# my_set = {} # Это пустой словарь
+# my_set = set() # Пустой set можно создать только так
+
+
+# my_dict = {'one': 'value1', 'two': 'value2'}
+# print(my_dict['one'])
+# print(my_dict['two'])
+# print(len(my_dict))
+# my_dict['tree'] = 'value3'
+# my_dict[2] = 'value4'
+# my_dict[False] = 'value5'
+# my_dict[(1, 2)] = 'value6'
+# my_dict[2.42] = 'value7'
+
+# print(my_dict)
+
+
